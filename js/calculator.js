2e21f1b5a0a847ea7c4e7feb7e8fc98542fdcfdf
Aaron Moradi <moradi.aaron@gmail.com>
2014-10-14 21:32:27 -0500

    js tidying up

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,6 @@
 /*
  *
- *	Calculator JS
+ *	Simple Calculator JS
  *	
  *	Aaron Moradi - 10 / 2014
  *	aaronmoradi.com
@@ -9,9 +9,6 @@
 
 var buttons = document.getElementsByTagName('button'); // calculator buttons
 
-/*
- *	
- */
 function calcClick()
 {
 
@@ -20,84 +17,40 @@ function calcClick()
 		totScrVal 		= totalScreen.value, 				// total screen value (string)
 		totScrArray 	= totScrVal.split(""); 
 
-	/*
-	 *	click equals  
-	 */
+	/*----------------*/
+	/*- Click Equals -*/ 
+	/*----------------*/
 	if (currentValue == "=")
 	{
-
-		/*
-		 *	if total screen is populated
-		 */
+		// total screen populated
 		if(totScrVal != "")
 		{
-			/*
-			 * if screen has ^ and is longer than 2 characters, tokenize, add to Math.pow(-3,3);
-			 */
+			// replace E with (2.7)
+			if( (totScrVal.indexOf('E') >= 0)) {
+				totScrVal = totScrVal.replace(/E/g,'(2.718281828459045)');
+			}
+			// replace PI with (PI)
+			if( (totScrVal.indexOf('PI') >= 0)) {
+				totScrVal = totScrVal.replace(/PI/g,'(PI)');
+			} 
+			// if has sqrt sign replace with sprt
 			if( (totScrVal.indexOf('\u221A') >= 0) && (totScrVal.length > 3) && ((totScrVal.charAt((totScrVal.length)-1)) == ')' ) )
 			{
-				
-				totScrVal = totScrVal.replace(/\u221A/g,'math.sqrt');
-				totScrVal = totScrVal.replace(/tan/g,'math.tan');
-				totScrVal = totScrVal.replace(/cos/g,'math.cos');
-				totScrVal = totScrVal.replace(/sin/g,'math.sin');
-				totalScreen.value = eval(totScrVal);	
+				totScrVal = totScrVal.replace(/\u221A/g,'sqrt');
 			}
-					/*
-					 *	case insensitive and global (all matches)
-					 *	dont let 2+3^2-1 compute -1 in the ^ 
-					 
-					var re = /[0-9.-]+\^[0-9.-]+/i,
-						matches = totScrVal.match(re); // ["9^2", "10^10"]
-					alert(matches);
-					/*
-					 *	compute power
-					 
-					function raisePow(x,y) {
-						return Math.pow(x,y)
-					}
-					/*
-					 *	compute each ^ expression
-					 
-					matches.forEach(function(item) 
-					{
-						var power = item.indexOf('^'),
-							x = item.slice(0, power),
-							y = item.slice(power+1),
-							computedPow = raisePow(x, y);
-						
-						/*	
-						 *	var strComputedPow = computedPow.toString();
-						 
-						rplTotScrVal = totScrVal.replace(item, computedPow);
-						totScrVal = rplTotScrVal;
-					});
-				}
-				totalScreen.value = math.eval(totScrVal); 
-			}*/
-			/*else 
-			{
-				/*
-				 *	added exception for 2^
-				 */
-				totalScreen.value = math.eval(totScrVal); 
-			/*}*/	
-
-			
+			// compute total screen
+			totalScreen.value = math.eval(totScrVal); 			
 		}
-		/* 
-		 *	total screen is empty
-		 */
+		// total screen empty
 		else 
 		{
 			totalScreen.value = "";
 		}	
-
 	} // end click equals
 
-	/*
-	 *	click delete  
-	 */
+	/*----------------*/
+	/*- Click Delete -*/ 
+	/*----------------*/
 	else if (currentValue == "delete")
 	{
 		var totScrArr 			= totalScreen.value,
@@ -107,29 +60,19 @@ function calcClick()
 			totalScreen.value 	= totScrArr;
 	}
 
-
-	/*
-	 *	click clear
-	 */
+	/*---------------*/
+	/*- Click Clear -*/ 
+	/*---------------*/
 	else if (currentValue == "clear")
 	{
 		totalScreen.value = "";
 	}
 
-	/*
-	 *	click everything except equals, clear, and delete
-	 */
+	/*--------------------------*/
+	/*- Click !=, !clear, !del -*/ 
+	/*--------------------------*/
 	else 
-	{
-		if(currentValue == "PI")
-		{
-			currentValue = Math.PI;
-		}
-		if(currentValue == "e")
-		{
-			currentValue = Math.E;
-		}  
-		
+	{		
 		if (totScrVal) // is total screen is already populated 
 		{
 		totalScreen.value = totalScreen.value + currentValue;
@@ -141,9 +84,9 @@ function calcClick()
 	}
 } // end calcClick()
 
-/*
- *	add event listener to buttons 
- */
+/*---------------------------------*/
+/*- Add Event Listener to Buttons -*/ 
+/*---------------------------------*/
 if (addEventListener) // non-IE, IE 9 +
 {
 	for (var i=0; i < buttons.length; i++) 
